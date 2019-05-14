461642e00533f23fc59c3dc2ca9a0f042fb3e13e
Heath Banner <God@Heaths-MBP.lan1>
2019-05-14 18:28:20 -0400

    Working login/signup and page for both

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var Strategy = require('passport-local').Strategy;
 require('./config/passport')(app);
-// var {ensureAuthenticated} = require('./config/auth');
 
 app.engine('handlebars', exphbs({defaultLayout: "main"}));
 app.set('view engine', 'handlebars');
@@ -29,10 +28,10 @@ app.use((error, req, res, next) => {
       user: req.user,
       error
     });
-  });
+});
 
 app.post('/login', passport.authenticate('local', 
-{successRedirect: '/lol', failureRedirect: '/why', failureFlash: true }
+{successRedirect: '/city', failureRedirect: '/login', failureFlash: true }
 ));
 
 var PORT = process.env.PORT || 8080;
