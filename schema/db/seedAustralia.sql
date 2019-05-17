aed0586652260ffd85299ded0cf5307276af7a05
mcnamarakc <mcnamarakc@users.noreply.github.com>
2019-05-16 22:10:40 -0400

    created new DB columns, added an activity to each country in NA, SA and australia

--- schema/db/seedAustralia.sql
+++ schema/db/seedAustralia.sql
@@ -1,6 +1,6 @@
 USE countries_db;
 
-INSERT INTO countries (continent, country, bty, foods, religions, brief_history, facts, fun_fact)
+INSERT INTO countries (continent, country, bty, foods, religions, brief_history, facts, fun_fact, activity_name, activity_description, activity_media_url)
 VALUES("Australia", 
 "Australia",
 "During the cool and dry season between November and early April",
@@ -8,10 +8,13 @@ VALUES("Australia",
 "52.1% Christian, 22.6% Catholic, 13.3% Anglican, 8.2% other religion.",
 "The history of Australia is the history of the area and people of the Commonwealth of Australia with its preceding Indigenous and colonial societies. Aboriginal Australians arrived on the Australian mainland by sea from Maritime Southeast Asia between 40,000 and 70,000 years ago. The artistic, musical and spiritual traditions they established are among the longest surviving such traditions in human history. Slowly, more and more of the settlers were not convicts. Sometimes you will still hear people refer to Australia as being started by a penal colony. Six colonies were formed in Australia: New South Wales, 1788; Tasmania, 1825; Western Australia, 1829; South Australia, 1836; Victoria, 1851; and Queensland, 1859.",
 "Australia, the smallest continent and one of the largest countries on Earth, lying between the Pacific and Indian oceans in the Southern Hemisphere. Australia has a range of different landscapes, including urban areas, mountain ranges, deserts and rain forests. Australia is home to a variety of unique animals, including the koala, kangaroo, emu, kookaburra and platypus.The Australian Alps get more snow than the Swiss Alps. Tasmania has the cleanest air in the world. The Great Barrier Reef is the largest eco-system in the world. Australia has over 60 separate wine regions. Fraser Island is the largest sand island in the world.",
-" 90% of Australians live on the coast"
+" 90% of Australians live on the coast",
+"Royal Botanic Gardens Victoria",
+"Visit Melbourne's inner-city oasis. Over 10,000 plant species from around the world are presented in a kaleidoscope of colour and texture. Sweeping lawns, tranquil lakes and majestic trees are home to an amazing range of wildlife.",
+"https://www.tripadvisor.com/Attraction_Review-g255100-d256846-Reviews-Royal_Botanic_Gardens_Victoria-Melbourne_Victoria.html#photos;aggregationId=101&albumid=101&filter=7&ff=367920609"
 );
 
-INSERT INTO countries (continent, country, bty, foods, religions, brief_history, facts, fun_fact)
+INSERT INTO countries (continent, country, bty, foods, religions, brief_history, facts, fun_fact, activity_name, activity_description, activity_media_url)
 VALUES("Australia", 
 "New Zealand",
 "Summer - December to February",
@@ -19,5 +22,8 @@ VALUES("Australia",
 "Catholicism (12.61%), Anglicanism (11.79%), Presbyterianism (8.47%), Other Christianity (15.14%), Hinduism (2.11%), Buddhism (1.50%), Islam (1.18%), Other religions (1.53%)",
 "The trees are greenhistory of New Zealand dates back at least 700 years to when it was discovered and settled by Polynesians, who developed a distinct Māori culture centred on kinship links and land. The first European explorer to sight New Zealand was Dutch navigator Abel Tasman on 13 December 1642. In 1840, the Treaty of Waitangi was signed, an agreement between the British Crown and Maori. It established British law in New Zealand and is considered New Zealand’s founding document and an important part of the country's history. The building where the treaty was signed has been preserved.",
 "New Zealand was the first country to give women the vote. Nowhere in New Zealand is more than 128 km (80 miles) from the sea. It is an island (two to be precise) and rather long and slender. The result of that is you are never more than an hour or two from the sea, no matter where you are. You could be skiing in the Southern Alps and splashing in the ocean in the same day. New Zealand has the world’s largest hot water spring. Frying Pan Lake near Rotorua. New Zealand has the southern most vineyards in the world. New Zealand contains more bookstores and golf courses per head of population than any other country.",
-"New Zealand has the world’s longest English language place name: Taumatawhakatangihangakoauauotamateapokaiwhenuakitanatahu. Roughly translated it means “The summit where Tamatea, the man with the big knees, the climber of mountains, the land-swallower who travelled about, played his nose flute to his loved one.” It is locally known as Taumata."
+"New Zealand has the world’s longest English language place name: Taumatawhakatangihangakoauauotamateapokaiwhenuakitanatahu. Roughly translated it means “The summit where Tamatea, the man with the big knees, the climber of mountains, the land-swallower who travelled about, played his nose flute to his loved one.” It is locally known as Taumata.",
+"Wai-O-Tapu Thermal Wonderland",
+"'One of the 20 Most Surreal Places in the World'-Trip Advisor THOUSANDS OF YEARS IN THE MAKING, explore spectacular nature at New Zealand's most colourful natural volcanic area. Walk at your own pace - view the unique Champagne Pool, Sinter Terrace formations, Steaming Ground, Bubbling Mud, Expansive Vistas, Huge Volcanic Craters. The Lady Knox Geyser erupts daily at 10.15am. Excellent facilities at modern Visitor Centre.",
+"https://www.tripadvisor.com/Attraction_Review-g255111-d258116-Reviews-Wai_O_Tapu_Thermal_Wonderland-Rotorua_Rotorua_District_Bay_of_Plenty_Region_North_.html#photos;aggregationId=101&albumid=101&filter=7&ff=365602538"
 );
