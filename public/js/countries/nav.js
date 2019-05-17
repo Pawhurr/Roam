81b648d7d4cc0e0f6556c0a7a3f02174a0361ac0
Heath Banner <God@Heaths-MacBook-Pro.local>
2019-05-17 13:33:05 -0400

    fixed animation lag

--- public/js/countries/nav.js
+++ public/js/countries/nav.js
@@ -1,17 +1,29 @@
 
-$('#nav-box').on('click', function() {
-    $('#nav-box').css({
-        display: 'none'
-    });
+$('.toggle').on('click', function() {
+    $('.toggle').toggleClass('active');
 
-    $('#navbar').css({
-        display: 'block'
-    });
-});
+    // $('.modal').toggle();
+    
+    if($('.modal').hasClass('active')) {
+       
+        $('.country-imgs').animate({
+            opacity: '0'
+        }, 300);
 
-$('#li').on('click', function() {
-    $('#country-list').css({
-        display: 'block'
-    });
+        $('.modal').animate({
+            left: '-200%'
+        },1000, function() {});
+
+
+    } else {
+        $('.modal').animate({
+            left: '0%'
+        },1000, function() {});
+        setTimeout(function(){$('.country-imgs').animate({
+            opacity: '1'
+        }, 1500)}, 980);
+    }
+
+    $('.modal').toggleClass('active');
 });
 
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.engine('handlebars', exphbs({defaultLayout: "main"}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/node_modules"));
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
