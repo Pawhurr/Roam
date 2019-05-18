11a9a62476ebfa20626b7dd560878cf311b279b9
Paul Whurr <pawhurr@gmail.com>
2019-05-17 20:34:23 -0400

    africa seed changes

--- schema/db/seedAfrica.sql
+++ schema/db/seedAfrica.sql
@@ -1,6 +1,6 @@
 USE countries_db;
 
-INSERT INTO countries (continent, country, bty, foods, religions, brief_history, facts, fun_fact)
+INSERT INTO countries (continent, country, bty, foods, religions, brief_history, facts, fun_fact, activity_name, activity_description, activity_media_url)
 VALUES("Africa",
 "Morocco",
 "spring (mid-March to May) and Autumn (September to October)",
@@ -8,10 +8,13 @@ VALUES("Africa",
 "Islam (93%), Christianity, Bahá'í community, Jews.",
 "The recorded history of Morocco begins with the Phoenician colonization of the Moroccan coast between the 8th and 6th centuries BCE, although the area was inhabited by indigenous Berbers for some two thousand years before that. Half a century later, the Moroccan state was established by the Idrisid dynasty.",
 "Morocco is one of the oldest recognized countries in Africa, with its first declaration of sovereignty dating back to the year 788. The city of Fez is a UNESCO world heritage site. Visitors from the UK, EU, Australia, Canada, USA, and Japan are allowed to stay in Morocco for up to three months without a Visa. Morocco encompasses desert (Sahara), mountainous (Atlas, Rif) and coastal (Atlantic, Mediterranean) regions.",
-"The Karawan mosque in Fez is the oldest university in the world built by Fatima al-Fihr"
+"The Karawan mosque in Fez is the oldest university in the world built by Fatima al-Fihr",
+"Jardin Majorelle",
+"The Jardin Majorelle in Marrakech is one of the most visited sites in Morocco. It took French painter Jacques Majorelle (1886-1962) forty years of passion and dedication to create this enchanting garden in the heart of the 'Ochre City'.",
+"http://jardinmajorelle.com/ang/"
 );
 
-INSERT INTO countries (continent, country, bty, foods, religions, brief_history, facts, fun_fact)
+INSERT INTO countries (continent, country, bty, foods, religions, brief_history, facts, fun_fact, activity_name, activity_description, activity_media_url)
 VALUES("Africa",
 "South Africa",
 "Summer months between November and February, For whale watching is between July and November.",
@@ -19,10 +22,13 @@ VALUES("Africa",
 "Christians(81%), Unaffiliated (10.9%), Traditional african religion(4.4%), Islam (1.6%), Hinduism (1.0%), Judaism (0.1%), Other religions (2.7%)",
 "Indian slaves from the Dutch colonies had been introduced into the Cape area of South Africa by the Dutch settlers in 1654. By the end of 1847, following annexation by Britain of the former Boer republic of Natalia, nearly all the Boers had left their former republic, which the British renamed Natal.",
 "South Africa has 11 official languages, including Zulu, Afrikaans, Xhosa and English. South Africa has the largest economy of any African country. South Africa has had an unsettled history, with apartheid. Conservation is an important issue in South Africa. It has lost many natural habitats due to deforestation and overpopulation.",
-"The first human heart transplant was performed in a Cape Town hospital in 1967"
+"The first human heart transplant was performed in a Cape Town hospital in 1967",
+"Table Mountain Aerial Cableway",
+"The Table Mountain Aerial Cableway is a cable car transportation system offering visitors a five-minute ride to the top of Table Mountain in Cape Town, South Africa. It is one of Cape Town's most popular tourist attractions with approximately one million people a year using the Cableway.[4] In January 2019, the Cableway welcomed its 28 millionth visitor.",
+"https://www.tablemountain.net/"
 );
 
-INSERT INTO countries (continent, country, bty, foods, religions, brief_history, facts, fun_fact)
+INSERT INTO countries (continent, country, bty, foods, religions, brief_history, facts, fun_fact, activity_name, activity_description, activity_media_url)
 VALUES("Africa",
 "Egypt",
 "Best time to visit is from October to April.",
@@ -30,10 +36,13 @@ VALUES("Africa",
 "Sunni Muslims (80-94%), Coptic Christians (6% to 20%)",
 "The history of ancient Egypt spans the period from the early prehistoric settlements. By 1600 BC, the Hyksos had successfully moved south in central Egypt, eliminating the Abydos Dynasty and directly threatening the Sixteenth Dynasty.",
 "World's oldest dress was found here. The Great Pyramids was not built by slaves. Greater Cairo is the largest city in Africa and the Middle East. The most popular sport in Egypt is football.",
-"The Egyptians invented the 365 days a year Calender"
+"The Egyptians invented the 365 days a year Calender",
+"Mahmya Island",
+"Totally inspired from its natural surroundings, Mahmya is an eco-tourism haven bordered by virgin blue waters awaiting exploration and a white sandy beach inviting relaxation.  Accompanied with fine food, great music and a fun crowd, Mahmya’s an exotic escape that will change your idea of the perfect day in the Red Sea and give you memories that will last forever!",
+"http://www.mahmya.com/"
 );
 
-INSERT INTO countries (continent, country, bty, foods, religions, brief_history, facts, fun_fact)
+INSERT INTO countries (continent, country, bty, foods, religions, brief_history, facts, fun_fact, activity_name, activity_description, activity_media_url)
 VALUES("Africa",
 "Tanzania",
 "Dry season from late June to October. Wildebeest migration in Serengeti is June and July and calving is late January to February.",
@@ -41,10 +50,13 @@ VALUES("Africa",
 "Christianity is the largest religion in Tanzania, and Muslim and Animist minorities",
 "On 26 April 1964, Tanganyika united with Zanzibar to form the United Republic of Tanganyika and Zanzibar. The country was renamed the United Republic of Tanzania on 29 October of that year. The name Tanzania is a blend of Tanganyika and Zanzibar and previously had no significance.",
 "Swahili and English are the two official languages of Tanzania. Mount Kilimanjaro is the highest free-standing mountain in the world. Over 1/4 of the land belongs to wildlife parks and conservation areas",
-"Tanzania has 2 capital cities. Dar Es Salaam is the former capital which has government offices. Dodoma is the present capital where the Presidential office and a few government ministries are located."
+"Tanzania has 2 capital cities. Dar Es Salaam is the former capital which has government offices. Dodoma is the present capital where the Presidential office and a few government ministries are located.",
+"Cheetahs Rock",
+"Cheetah’s Rock is a unique place where people and animals interact directly – face to face, eye to eye, fur to skin.  In addition to meeting Jenny’s rescued wild animals on the tour you will receive substantial information about animal welfare and wildlife conservation. You will learn more about needed protection and conservation of endangered species and more on the behavior of various animal species. You will hear how it is possible to get so close to wild animals using 100% positive reinforcement only animal handling. You are more than welcome to take as many photos as you wish.",
+"http://cheetahsrock.org/"
 );
 
-INSERT INTO countries (continent, country, bty, foods, religions, brief_history, facts, fun_fact)
+INSERT INTO countries (continent, country, bty, foods, religions, brief_history, facts, fun_fact, activity_name, activity_description, activity_media_url)
 VALUES("Africa",
 "Kenya",
 "Dry season from late June to October. The wildebeest migration in Masai Mara in July and remains until October.",
@@ -52,5 +64,8 @@ VALUES("Africa",
 "Christianity (84.8%), Islam (9.7 to 11.1%)",
 "The British Empire established the East Africa Protectorate in 1895, from 1920 known as the Kenya Colony. The independent Republic of Kenya was formed in 1964. It was ruled as a de facto one-party state by the Kenya African National Union (KANU), led by Jomo Kenyatta during 1963 to 1978.",
 "The two official languages in Kenya are English and Swahili, although there are dozens of other languages spoken in various parts of the country. Kenya shares Lake Victoria, the world's second largest fresh water lake, with Tanzania and Uganda. Kenya features many national parks and wildlife reserves, with safaris being a popular activity for visitors. Agriculture is important to Kenya's economy, especially tea, coffee and flowers. Large animals such as lions, buffalo, leopards, elephants and rhinoceros are present in Kenya.",
-"Apple’s Siri Originated from Swahili. IPhone friend received her name from a Swahili word, which means Secret?"
+"Apple’s Siri Originated from Swahili. IPhone friend received her name from a Swahili word, which means Secret?",
+"Haller Park",
+"Formerly known as Bamburi Nature Trail until 1999, Haller Park is a unique ecological restored quarry situated on the North Coast of Mombasa along the Mombasa Malindi road, next to the Bamburi Cement factory. Open to the public for the last 21 years, the Nature Park Sanctuary is home to a variety of plant and animal species that serve as attractions to local and international visitors from all walks of life.",
+"http://www.owenandmzee.com/omweb/hallerpark.html"
 );
